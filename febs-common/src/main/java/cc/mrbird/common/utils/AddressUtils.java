0a9150a418be718853b7d54a641d795baa492935
mrbird <852252810@qq.com>
2019-06-17 19:18:35 +0800

    修复流未关闭

--- febs-common/src/main/java/cc/mrbird/common/utils/AddressUtils.java
+++ febs-common/src/main/java/cc/mrbird/common/utils/AddressUtils.java
@@ -1,49 +1,41 @@
 package cc.mrbird.common.utils;
 
+import org.apache.commons.io.FileUtils;
 import org.lionsoul.ip2region.DataBlock;
 import org.lionsoul.ip2region.DbConfig;
 import org.lionsoul.ip2region.DbSearcher;
 import org.lionsoul.ip2region.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Objects;
 
+/**
+ * @author MrBird
+ */
 public class AddressUtils {
-
-    private AddressUtils() {
-    }
 
     private static Logger log = LoggerFactory.getLogger(AddressUtils.class);
 
     public static String getCityInfo(String ip) {
+        DbSearcher searcher = null;
         try {
-            Resource resource = new ClassPathResource("ip2region/ip2region.db");
-            File file = resource.getFile();
+            String dbPath = AddressUtils.class.getResource("/ip2region/ip2region.db").getPath();
+            File file = new File(dbPath);
             if (!file.exists()) {
-                log.error("Error: Invalid ip2region.db file");
+                String tmpDir = System.getProperties().getProperty("java.io.tmpdir");
+                dbPath = tmpDir + "ip.db";
+                file = new File(dbPath);
+                FileUtils.copyInputStreamToFile(Objects.requireNonNull(AddressUtils.class.getClassLoader().getResourceAsStream("classpath:ip2region/ip2region.db")), file);
             }
             int algorithm = DbSearcher.BTREE_ALGORITHM;
             DbConfig config = new DbConfig();
-            DbSearcher searcher = new DbSearcher(config, file.getPath());
+            searcher = new DbSearcher(config, file.getPath());
             Method method = null;
-            switch (algorithm) {
-                case DbSearcher.BTREE_ALGORITHM:
-                    method = searcher.getClass().getMethod("btreeSearch", String.class);
-                    break;
-                case DbSearcher.BINARY_ALGORITHM:
-                    method = searcher.getClass().getMethod("binarySearch", String.class);
-                    break;
-                case DbSearcher.MEMORY_ALGORITYM:
-                    method = searcher.getClass().getMethod("memorySearch", String.class);
-                    break;
-                default:
-                    method = searcher.getClass().getMethod("memorySearch", String.class);
-                    break;
-            }
+            method = searcher.getClass().getMethod("btreeSearch", String.class);
             DataBlock dataBlock = null;
             if (!Util.isIpAddress(ip)) {
                 log.error("Error: Invalid ip address");
@@ -51,9 +43,16 @@ public class AddressUtils {
             dataBlock = (DataBlock) method.invoke(searcher, ip);
             return dataBlock.getRegion();
         } catch (Exception e) {
-            log.error("获取地址信息异常：{}", e);
+            log.error("获取地址信息异常", e);
+        } finally {
+            if (searcher != null) {
+                try {
+                    searcher.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return "";
     }
-
 }
